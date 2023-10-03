b635835979f7ad1fbf01d211b97af70d24f9364a
CodeDesignPlus <codedesignplus@outlook.com>
2023-10-03 07:26:05 -0500

    style(netcore): update import

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,4 +1,4 @@
-import { Publish } from "./publish";
+import { Publish } from './publish';
 
 //To execute, it's necessary to have tokens set as arguments
 //npx ts-node --files ./scripts/index.ts --npm-token=token1 --npm-gh-token=token2
