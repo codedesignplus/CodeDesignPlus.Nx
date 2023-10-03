d05c6cf92f5ad4d2f243e8ca4d9ca1782e917625
CodeDesignPlus <codedesignplus@outlook.com>
2023-10-03 07:50:28 -0500

    refactor(netcore): remove files generator

--- packages/netcore/src/generators/library/files/tests/CodeDesignPlus.Net.Library.Test/Helpers/Models/AppSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿using CodeDesignPlus.Net.Library.Options;
-
-namespace CodeDesignPlus.Net.Library.Test.Helpers.Models;
-
-public class AppSettings
-{
-    public LibraryOptions? Library { get; set; }
-}
